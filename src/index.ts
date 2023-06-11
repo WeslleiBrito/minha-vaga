50c73d37c981faf14a65abb293bd563b2d44ff0e
WeslleiBrito <wesllei.wbs@gmail.com>
2023-06-11 13:37:54 -0300

    Criado a variavel para verificar possiveis correçoes por parte do cliente

--- src/index.ts
+++ src/index.ts
@@ -67,26 +67,122 @@ app.post('/usuarios', (req: Request, res: Response) => {
 
 // Incluir novo processo
 app.post('/aplicacao', (req: Request, res: Response) => {
-    const { jobName, companyName, applicationDate, jobRequirements, processStatus }: TApplication = req.body
+    const { jobName, companyName, applicationDate, jobRequirements, processStatus } = req.body
+
+    const values = [
+        jobName,
+        companyName,
+        applicationDate,
+
+    ]
+
+    const nameValues = [
+        "jobName",
+        "companyName",
+        "applicationDate"
+    ]
 
     const query = 'INSERT INTO applications (job_name, company_name, application_date, job_requirements, process_status) VALUES (?, ?, ?, ?, ?)'
-    
-    db.run(query, [jobName, companyName, applicationDate, jobRequirements.toString(), processStatus], (err: any) => {
 
-        if(err){
-            console.error('Erro ao inserir a nova aplicação da vaga!')
+    let propertyNameSolution = ""
 
-            res.status(500).json(
-                {
-                    error: "Erro ao inserir a nova aplicação"
+    const possibleSolutions = {
+
+        jobRequirements: {
+
+            portuguese: [
+                "Verifique o tipo de dado informado na propriedade `jobRequirements`.",
+                "Verifique se a propriedade `jobRequirements` foi passada.",
+                "Verifique se a sintaxe da propriedade `jobRequirements` foi escrita corretamente.",
+                "Verifique se o array passado tem algum valor dentro dele."
+            ],
+
+            english : [
+                "Check the data type informed in the `jobRequirements` property.",
+                "Check if the `jobRequirements` property is passed.",
+                "Check that the syntax of the `jobRequirements` property is written correctly.",
+                "Check if the passed array has any value inside it."
+
+            ]
+
+        }, 
+
+        itemJobRequirements: {
+
+            portuguese: [
+                "Verifique se todos os requerimentos são do tipo string.",
+                "Verifique se existe algum requerimento vazio."
+            ],
+
+            english: [
+                "Verify that all requests are of type string.",        
+                "Check if there are any empty requirements."
+            ]
+        },
+
+        typeValueGeneric : (nameValue: string) => {
+            return {
+                portuguese: [
+                    `Verifique se a propriedade '${nameValue}' foi informada, ou se o tipo passado é uma string`
+                ],
+
+                english: [
+                    `Check if the property '${nameValue}' was informed, or if the type passed is a string`
+                ]
+            }
+        }
+    }
+
+    try {
+
+        if(typeof jobRequirements === "object" && jobRequirements.length > 0){
+
+            jobRequirements.map((requirement: any) => {
+
+                if(typeof requirement !== "string" || !requirement){
+                    propertyNameSolution = "typeItensRequirements"
+                    throw new Error(JSON.stringify(possibleSolutions.itemJobRequirements))
                 }
-            )
+            })   
 
-            return
+        }else{
+
+            throw new Error(JSON.stringify(possibleSolutions.jobRequirements))
         }
 
-        res.status(201).send('Cadastro efetuado com sucesso!')
-    })
+        values.map((value, index) => {
+            if(!value){
+                throw new Error(JSON.stringify(possibleSolutions.typeValueGeneric(nameValues[index])))
+            }
+        })
+
+        db.run(query, [jobName, companyName, applicationDate, jobRequirements.toString(), processStatus], (err: any) => {
+
+            if(err){
+                console.error('Erro ao inserir a nova aplicação da vaga!')
+    
+                res.status(500).json(
+                    {
+                        error: "Erro ao inserir a nova aplicação"
+                    }
+                )
+    
+                return
+            }
+    
+            res.status(201).send('Cadastro efetuado com sucesso!')
+        })
+
+    } catch (error:any) {
+    
+        res.status(400).send(
+            {
+                possibleSolutions: JSON.parse(error.message) 
+            }
+        )
+    }
+    
+    
 })
 
 app.listen(3003, () => {
